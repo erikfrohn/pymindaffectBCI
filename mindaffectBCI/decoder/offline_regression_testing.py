ff4cefa0ec485bc6da57204e7e60172226f98ac4
Freek <freek.hens@ru.nl>
2022-05-23 16:16:23 +0200

    fix bug

--- mindaffectBCI/decoder/offline_regression_testing.py
+++ mindaffectBCI/decoder/offline_regression_testing.py
@@ -212,7 +212,8 @@ if __name__=="__main__":
      StopThresh(P)  0.79  0.79  0.62  0.44  0.38  0.38  0.39  0.39   SSAE 15.6')
 
     with open('metrics.txt', 'w') as f:
-        f.write(res)
+        if not res:
+            f.write(res)
         f.write('BASELINE: 336f594\n Ave-DC\n\
                 IntLen   100   201   277   378   478   554   655   756\n\
                 Perr  0.72  0.41  0.39  0.27  0.26  0.20  0.18  0.15   AUDC 35.2\n\
@@ -232,7 +233,8 @@ if __name__=="__main__":
      StopThresh(P)  0.89  0.89  0.89  0.89  0.63  0.59  0.60  0.61   SSAE 14.7")
 
     with open('metrics.txt', 'w') as f:
-        f.write(res)
+        if not res:
+            f.write(res)
         f.write('BASELINE: 336f594\n Ave-DC\n\
                 IntLen   100   201   277   378   478   554   655   756\n\
                 Perr  0.72  0.41  0.39  0.27  0.26  0.20  0.18  0.15   AUDC 35.2\n\
@@ -251,7 +253,8 @@ if __name__=="__main__":
      StopThresh(P)  0.86  0.82  0.77  0.63  0.49  0.44  0.44  0.44   SSAE 23.9")
 
     with open('metrics.txt', 'w') as f:
-        f.write(res)
+        if not res:
+            f.write(res)        
         f.write('BASELINE: 336f594\n Ave-DC\n\
                 IntLen   100   201   277   378   478   554   655   756\n\
                 Perr  0.72  0.41  0.39  0.27  0.26  0.20  0.18  0.15   AUDC 35.2\n\
