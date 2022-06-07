798076ac8bce9de28b430de07c912d57b95ba282
victorw271 <73604301+victorw271@users.noreply.github.com>
2022-06-07 18:23:51 +0200

    Update new_dataset_uploaded_pipeline.py
    new

--- pages/new_dataset_uploaded_pipeline.py
+++ pages/new_dataset_uploaded_pipeline.py
@@ -5,34 +5,63 @@ import altair as alt
 from pp import pp
 
 def app():
-    #st.header("New dataset uploaded pipeline")
-
-    # chart_data = pd.DataFrame(
-    #     [[1.10,0.80,0.68,0.60,0.50,0.40,0.40,0.45],[72,145,199,272,345,399,472,545]],
-    #     columns=['a'])
-    #
-    # st.line_chart(chart_data)
-
-    # #x = np.arange(500)
 
     dfs, column_names, columns_names2, columns_names2, path_list, time, mds = pp()
 
-    #[72,145,199,272,345,399,472,545]
-    #[1.10,0.80,0.68,0.60,0.50,0.40,0.40,0.45]
-    source = pd.DataFrame({
-        'Integration Length (samples)': dfs[4]['int_len'] ,
-        'Perr': dfs[4]['prob_err']
-    })
+    perr = []
+    perr_e = []
+    se = []
+    ste = []
 
-    c= alt.Chart(source).mark_line().encode(
-        x='Integration Length (samples)',
-        y='Perr'
-    )
+    id = 4
 
-    st.subheader("Analysis noisetag_bci_201029_1340_ganglion dataset")
-    st.altair_chart(c, use_container_width=True)
+    perr.append( dfs[id]['prob_err'] )
+    perr_e.append (  dfs[id]['prob_err_est'] )
+    se.append( dfs[id]['se'] )
+    ste.append(dfs[id]['st'])
 
-    st.write(pd.DataFrame({
-        'StopErr': dfs[4]['se'],
-        'StopThresh(P)': dfs[4]['st']
-    }))
+    new_perr = np.array(perr)
+    new_perr_e = np.array(perr_e)
+    new_se = np.array(se)
+    new_st = np.array(ste)
+
+    file = [path[2] for path in path_list]
+    name = file[id]
+
+    input = np.array([new_perr, new_perr_e, new_se, new_st]).reshape(4,30)
+
+    source = pd.DataFrame(np.transpose(input), columns=['Perr', 'Perr_est', 'Se', 'St'])
+    st.header(name)
+    #line = alt.Chart(source).mark_line().encode(x='Int_len',  y='Score')
+    #st.altair_chart(line)
+ 
+    st.line_chart(source)
+
+    perr = []
+    perr_e = []
+    se = []
+    ste = []
+
+    id = 13
+
+    perr.append( dfs[id]['prob_err'] )
+    perr_e.append (  dfs[id]['prob_err_est'] )
+    se.append( dfs[id]['se'] )
+    ste.append(dfs[id]['st'])
+
+    new_perr = np.array(perr)
+    new_perr_e = np.array(perr_e)
+    new_se = np.array(se)
+    new_st = np.array(ste)
+
+    file = [path[2] for path in path_list]
+    name = file[id]
+
+    input = np.array([new_perr, new_perr_e, new_se, new_st]).reshape(4,30)
+
+    source = pd.DataFrame(np.transpose(input), columns=['Perr', 'Perr_est', 'Se', 'St'])
+    st.subheader(name)
+    #line = alt.Chart(source).mark_line().encode(x='Int_len',  y='Score')
+    #st.altair_chart(line)
+ 
+    st.line_chart(source)
