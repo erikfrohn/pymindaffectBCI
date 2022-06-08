005d60d76355eb03942ce02097068db873cf9b26
victorw271 <73604301+victorw271@users.noreply.github.com>
2022-06-09 00:27:18 +0200

    Update data_upload.py
    documentation

--- pages/data_upload.py
+++ pages/data_upload.py
@@ -4,12 +4,14 @@ import pandas as pd
 from PIL import  Image
 
 def app():
-    #st.markdown("## Data Upload")
+    '''it inspects your raw csv file'''
+    
     st.title("Tracking Dashboard")
     display = Image.open('MindAffect_Logo.png')
     display = np.array(display)
     st.image(display, width=400)
-    # Upload the dataset and save as csv
+    
+    # it uploads the dataset and save as csv
     st.markdown("### Upload a csv file for analysis")
     st.write("\n")
     uploaded_file = st.file_uploader("Choose a file", type = ['csv', 'xlsx'])
@@ -22,8 +24,7 @@ def app():
             data = pd.read_excel(uploaded_file)
 
     if st.button("Load Data"):
-        
-        # Raw data 
+        # it processes the raw data 
         st.dataframe(data)
         data.to_csv('data/main_data.csv', index=False)
         numeric_cols = data.select_dtypes(include=np.number).columns.tolist()
