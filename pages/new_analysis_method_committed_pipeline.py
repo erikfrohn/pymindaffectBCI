b210bda2103b30efc31c04d088d43bdebbb08e5d
victorw271 <73604301+victorw271@users.noreply.github.com>
2022-06-08 23:59:52 +0200

    Update new_analysis_method_committed_pipeline.py
    documentation

--- pages/new_analysis_method_committed_pipeline.py
+++ pages/new_analysis_method_committed_pipeline.py
@@ -5,37 +5,24 @@ import altair as alt
 from pp import pp
 
 def app():
+    
+    '''it plots the average audc of kaggle, lowlands, plos_one over the commits you make in order:
+    the generic variables are preprocessed until they are formatted correctly for the charts'''
+    
     st.header("Performance tracking")
 
-    # df = pd.DataFrame([[20.500, 15.513, 90.667], [57.833, 23.077, 34.667],[46.000, 32.436, 30.333],[36.667, 20.256, 25.222],[46.000, 28.718, 33.456]], columns=['kaggle', 'plos_one', 'lowlands'])
-    # df.index.name = "Dataset-ID"
-    # df.set_index([pd.Index([1, 2, 3, 4,5])])
-    # data = df.reset_index().melt('Dataset-ID')
-    # c = alt.Chart(data).mark_line().encode(
-    #     x='Dataset-ID',
-    #     y='value',
-    #     color='variable'
-    # )
-    #test
-
-    dfs, column_names, columns_names2, path_list, time  = pp() 
+    dfs, column_names, columns_names2, path_list, sha  = pp() 
 
     avg_k_audc = []
     avg_l_audc = []
     avg_p_audc = []
     
-    #col1, col2 = st.columns([5, 1])
-    #for i in range(len(dfs)):
-        #st.write(dfs[i])
-#         'SHA': sha
-#     }))
-    
-    #st.line_chart(dfs[0]['AUDC'])
-    
+    #it finds the csv names
     id = [path[2] for path in path_list]
 
     id_array = np.array([id])
-
+    
+    #it finds all indices of specific csv file names
     id_k = np.where(id_array == 'kaggle.csv')[1]
     
     id_l = np.where(id_array == 'lowlands.csv')[1]
@@ -46,21 +33,22 @@ def app():
     l_l = id_l.tolist()
     l_p = id_p.tolist()
     
-    #for i in range(0, len(dfs), 18):
+    #it finds all the occured csv files of a specific name
     for i in range(len(l_k)):
         id1 = id_k[i]
         id2 = id_l[i]
         id3 = id_p[i]
-        avg_k_audc.append( dfs[id1]['ave-AUDC']) #, dfs[18]['ave-AUDC'] )
-        avg_l_audc.append( dfs[id2]['ave-AUDC']) #, dfs[19]['ave-AUDC'] )
-        avg_p_audc.append( dfs[id3]['ave-AUDC']) #, dfs[21]['ave-AUDC'] )
+        avg_k_audc.append( dfs[id1]['ave-AUDC']) 
+        avg_l_audc.append( dfs[id2]['ave-AUDC'])
+        avg_p_audc.append( dfs[id3]['ave-AUDC']) 
 
     new_k_audc = np.array(avg_k_audc)
     new_l_audc = np.array(avg_l_audc)
     new_p_audc = np.array(avg_p_audc)
 
     input = np.array([new_k_audc[:,0], new_l_audc[:,0], new_p_audc[:,0]])
-
+    
+    #it creates the chart; first dataframe and secondly manipulations on the charts (title, x-axis and y-axis)
     source = pd.DataFrame(np.transpose(input),
                           columns=['Kaggle', 'Lowlands', 'Plos_one'], index=pd.RangeIndex(1, len(avg_k_audc)+1, name='Commit-ID'))
     source = source.reset_index().melt('Commit-ID', var_name='dataset', value_name='avg-AUDC')
@@ -72,28 +60,9 @@ def app():
     title='Average AUDC across commits'
 )
     st.altair_chart(line, use_container_width=True)
-
-    #source = source.reset_index().melt('Commit-ID', var_name='dataset', value_name='avg-AUDC')
-    #line = alt.Chart(source).mark_line(interpolate='basis').encode(
-    #   x='Commit-ID',
-    #    y='avg-AUDC',
-    #).properties(
-    #title='Average AUDC across commits'
-#)
-    #st.altair_chart(line, use_container_width=True)
-    # col1, col2 = st.columns(2)
-
-
-#     branch = []
-#     #time = []
-
-#     for i in mds:
-#         branch.append(i['branch'])
-#         #time.append(i['current_date_and_time'])
-
-#     sha = np.unique(np.array(time))
-
-#     col1, col2 = st.columns([2, 1])
-#     col1.write(pd.DataFrame({'Commit-ID': range(1, len(sha)+1),
-#         'SHA': sha
-#     }))
+    
+    #it creates a table with the commit ids and the corresponding sha keys
+    col1, col2 = st.columns([2, 1])
+    col1.write(pd.DataFrame({'Commit-ID': range(1, len(sha)+1),
+         'SHA': sha
+     }))
