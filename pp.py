20f6daf85c863db5a2e2b44a1c0f03fe888db392
victorw271 <73604301+victorw271@users.noreply.github.com>
2022-06-08 23:38:49 +0200

    Update pp.py

--- pp.py
+++ pp.py
@@ -41,14 +41,4 @@ def pp():
     column_names = dfs[0].columns.tolist()
     columns_names2 = dfs[4].columns.tolist()
 
-    list_csv2 = glob.glob('csv/**/metadata.csv')
-
-    mds = []
-
-    for i in (list_csv2):
-        md = pd.read_csv(i, index_col=False)
-        mds.append(md)
-
-    column_names3 = mds[0].columns.tolist()
-
-    return dfs, column_names, columns_names2, path_list, time, columns_names3, mds  
+    return dfs, column_names, columns_names2, path_list, time  
