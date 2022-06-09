3aab62a2f19eb8991b66e35969bad3d7393f205a
sgrts <104440147+sgrts@users.noreply.github.com>
2022-06-09 14:59:36 +0200

    Add documentation

--- app.py
+++ app.py
@@ -2,18 +2,13 @@
 from MultiPage import MultiPage
 from pages import data_upload, new_dataset_uploaded_pipeline, new_analysis_method_committed_pipeline
 
+#create an instance of the multipage class
 app = MultiPage()
 
-# # Title of the main page
-# display = Image.open('MindAffect_Logo.png')
-# display = np.array(display)
-# # st.image(display, width = 400)
-# col1, col2 = st.columns(2)
-# col1.image(display, width = 400)
-# col2.title("Tracking Dashboard")
-
+#add pages to the app using the add_page method from the Multipage class
 app.add_page("Upload file", data_upload.app)
 app.add_page("New dataset uploaded pipeline", new_dataset_uploaded_pipeline.app)
 app.add_page("New analysis method committed pipeline", new_analysis_method_committed_pipeline.app)
 
+#start the application by calling the run method from the MultiPage class
 app.run()
