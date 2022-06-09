3482a0d2f8cc1e11afa0e9e020da9ec7944726bc
sgrts <104440147+sgrts@users.noreply.github.com>
2022-06-09 16:41:53 +0200

    Fix

--- MultiPage.py
+++ MultiPage.py
@@ -1,41 +1,15 @@
 
-#import necessary libraries
+
 import streamlit as st
 
-"""
-A class that holds an array of all the pages in the application
-It's also used to create/edit the array and run a particular page
-"""
 class MultiPage: 
-  
-"""
-Constructor of the class, it creates the array that holds the references to the pages
-
-...
-
-Methods
--------
-
-add_page(self, title, func)
-         Appends a new page to the array created by the constructor
-         
-"""
 
     def __init__(self):
 
         self.pages = []
     
     def add_page(self, title, func):
-        """
-        Appends a new page to the array, using the name of the page and a reference to the app method function call.
-        
-        Parameters
-        ----------
-        title : str
-                Name of the page
-        func: 
-             Holds the definition of the function call to the app method of the particular page
-        """
+
         self.pages.append(
             { 
                 "title": title, 
@@ -44,9 +18,6 @@ add_page(self, title, func)
         )
 
     def run(self):
-        """
-        Calls the app function of the page seleected by the user
-        """
 
         page = st.sidebar.selectbox(
             'App Navigation', 
