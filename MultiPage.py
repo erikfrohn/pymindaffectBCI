7ec3856001708dd73c87570557ad40cee7d82353
sgrts <104440147+sgrts@users.noreply.github.com>
2022-06-09 16:43:21 +0200

    Fix2

--- MultiPage.py
+++ MultiPage.py
@@ -1,28 +1,61 @@
-
-
+# import necessary libraries
 import streamlit as st
 
-class MultiPage: 
+"""
+A class that holds an array of all the pages in the application
+It's also used to create/edit the array and run a particular page
+"""
 
-    def __init__(self):
 
-        self.pages = []
-    
-    def add_page(self, title, func):
+class MultiPage:
 
-        self.pages.append(
-            { 
-                "title": title, 
-                "function": func
-            }
-        )
 
-    def run(self):
+  """
+  Constructor of the class, it creates the array that holds the references to the pages
 
-        page = st.sidebar.selectbox(
-            'App Navigation', 
-            self.pages, 
-            format_func=lambda page: page['title']
-        )
+  ...
 
-        page['function']()
+  Methods
+  -------
+
+  add_page(self, title, func)
+         Appends a new page to the array created by the constructor
+
+  """
+
+
+  def __init__(self):
+    self.pages = []
+
+
+  def add_page(self, title, func):
+    """
+    Appends a new page to the array, using the name of the page and a reference to the app method function call.
+
+    Parameters
+    ----------
+    title : str
+            Name of the page
+    func:
+         Holds the definition of the function call to the app method of the particular page
+    """
+    self.pages.append(
+        {
+            "title": title,
+            "function": func
+        }
+    )
+
+
+  def run(self):
+    """
+    Calls the app function of the page seleected by the user
+    """
+
+    page = st.sidebar.selectbox(
+        'App Navigation',
+        self.pages,
+        format_func=lambda page: page['title']
+    )
+
+    page['function']()
